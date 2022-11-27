0bc194c33f2e58ba09c83012125a4e2caad1fbac
hochdyl <55688853+hochdyl@users.noreply.github.com>
2022-11-27 17:23:45 +0100

    Delete .yalc directory

--- .yalc/autobahn-ui/dist/components/form/checkbox/Checbox.stories.d.ts
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { CheckboxProps } from "./CheckboxComponent";
-declare const _default: {
-    title: string;
-    component: React.FC<CheckboxProps>;
-};
-export default _default;
-export declare const Default: any;
